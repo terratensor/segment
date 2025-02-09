fcac6b19a2bee445bd9e4b4c1308309e60e8b7e6
audetv <audetv@gmail.com>
2025-02-10 00:48:51 +0300

    тест для other rule

---
+++ rule/other_rule_test.go
@@ -0,0 +1,54 @@
+package rule
+
+import (
+	"testing"
+
+	"github.com/terratensor/segment/segment"
+)
+
+func TestOtherRule(t *testing.T) {
+	rule := OtherRule{}
+
+	tests := []struct {
+		name     string
+		split    segment.TokenSplit
+		expected bool
+	}{
+		{
+			name: "OTHER и RU",
+			split: segment.TokenSplit{
+				LeftAtoms:  []segment.Atom{{Text: "Δ", Type: segment.Other}},
+				Delimiter:  "",
+				RightAtoms: []segment.Atom{{Text: "P", Type: segment.Ru}},
+			},
+			expected: true,
+		},
+		{
+			name: "RU и OTHER",
+			split: segment.TokenSplit{
+				LeftAtoms:  []segment.Atom{{Text: "mβж", Type: segment.Ru}},
+				Delimiter:  "",
+				RightAtoms: []segment.Atom{{Text: "Δ", Type: segment.Other}},
+			},
+			expected: true,
+		},
+		{
+			name: "OTHER и RU",
+			split: segment.TokenSplit{
+				LeftAtoms:  []segment.Atom{{Text: "К", Type: segment.Other}},
+				Delimiter:  "",
+				RightAtoms: []segment.Atom{{Text: "ружка", Type: segment.Ru}},
+			},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := rule.Apply(tt.split)
+			if result != tt.expected {
+				t.Errorf("Expected %v, got %v", tt.expected, result)
+			}
+		})
+	}
+}
